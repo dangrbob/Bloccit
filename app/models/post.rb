8e4ae7dee607ef8cb0486698a2f5561630169037
Rob Bankey <rob.bankey@gmail.com>
2015-08-20 15:02:26 -0400

    assignment-38 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
+  scope :ordered_by_title, -> {reorder('title')}
+
+  scope :ordered_by_reverse_created_at, -> {reorder('created_at DESC')} 
+
   default_scope {order('created_at DESC')}
 end
+
