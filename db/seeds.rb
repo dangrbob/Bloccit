362e07ee4f2072fdc9e87c21c6c80b995002ce43
Rob Bankey <rob.bankey@gmail.com>
2015-08-20 13:58:31 -0400

    checkpoint-38 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 require 'faker'
 
+#Create Users
+5.times do 
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 #Create Posts
 50.times do
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
@@ -11,14 +25,21 @@ end
 posts = Post.all
 
 #Create Comments
-50.times do
+100.times do
   Comment.create!(
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'rob.bankey@gmail.com',
+  password: 'helloworld'
+)
+
 puts "File seeded"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-
