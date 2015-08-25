edadc48df664fafc1fdf3bb8bab06221c23c2a80
Rob Bankey <rob.bankey@gmail.com>
2015-08-25 18:39:30 -0400

    assignement-39 in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+  
   protect_from_forgery with: :exception
+  include Pundit
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError do |exception|
@@ -8,6 +10,8 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,5 +4,30 @@ class PostPolicy < ApplicationPolicy
     true
   end  
 
+  class Scope 
+    attr_reader :user, :scope
 
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise Pundit::NotAuthorizedError, "Must be logged in" unless user
+      if user.admin?
+        scope.all
+      elsif user.moderator?
+        scope.all  
+      else
+        scope.where(user: @current_user)
+      end
+    end
+  end
+
+  def update?
+    user.admin? or (record.user == @current_user)
+  end
+
+  
 end
+
