4024624b30b83673b9aca4b684c00ada1b382739
Rob Bankey <rob.bankey@gmail.com>
2015-08-30 20:08:50 -0400

    assignment 43 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,22 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  def markdown_body(body)
+    render_as_markdown(body)
+  end
+  
+  def markdown_title(title)
+    render_as_markdown(title)
+  end    
+
+  private
+
+    def render_as_markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fendced_code_blocks: true}
+    redcarpet= Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+
+  end
+
 end
