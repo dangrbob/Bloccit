bc2083590a8f86dd3dfb3d3c1dbaa4670809b8d3
Rob Bankey <rob.bankey@gmail.com>
2015-09-07 23:13:17 -0400

    work in progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
-    @comment = Comment.find(params[:comment_id])
+    @comments = Comment.find(params[:id])
     authorize @post
   end
 
